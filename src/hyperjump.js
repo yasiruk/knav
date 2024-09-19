f057f3f89c4b12f9799b5274343c5f988a574b41
Yasiru Kassapa <me@yasiru.me>
2024-09-19 22:34:17 +0200

    Tweak searching to be not limited to anchor tags, use DFS for searching

--- src/hyperjump.js
+++ src/hyperjump.js
@@ -1,54 +1,102 @@
+/**
+ * @let {Object | null} candidate - The current candidate element.
+ * @property {HTMLElement} node - The candidate element.
+ * @property {number} index - The index of the search text in the candidate element.
+ */
 let candidate = null;
 
-const fuzzySearch = (text, query) => {
-    const textLower = text.toLowerCase()
-    const queryLower = query.toLowerCase()
-    let index = 0
-    for (let i = 0; i < queryLower.length; i++) {
-        const c = queryLower[i]
-        index = textLower.indexOf(c, index)
-        if (index === -1) {
-            return -1
+
+const indexOf = (text, char, startAt) => {
+    for (let i = startAt; i < text.length; i++) {
+        if (text[i].toLowerCase() === char) {
+            return i
         }
     }
+    return -1
+}
+
+const simpleSearch = (hayStack, needle) => {
+    return hayStack.toLowerCase().indexOf(needle)
+}
+
+const fuzzySearch = (hayStack, needle) => {
+    let index = 0
+    for (let i = 0; i < needle.length; i++) {
+        const c = needle[i]
+        const oldIndex = indexOf(hayStack, c, index)
+        if (oldIndex === -1) {
+            // console.log(`${c}:na:"${hayStack}`)
+            return -1
+        } else if (index - oldIndex > 10) {
+            return -1
+        }
+        index = oldIndex
+    }
+    // console.log(`${needle}:${index}:"${hayStack}`)
     return index
 }
-const find = (text) => {
-    const anchors = Array.from(document.querySelectorAll('a'))
-        .map((a) => {
-            return {
-                node: a,
-                index: fuzzySearch(a.innerText, text)
-            }
-        })
-        .filter((a) => a.index !== -1)
-        .sort((a, b) => a.index - b.index)
 
-
-    console.log(candidate, anchors)
-    if (candidate !== null) {
-        // remove the highlight css class
-        candidate.classList.remove('searcher_candidate')
-    }
-
-    candidate = anchors[0]?.node ?? null
-    // add the highlight css class
-    if (candidate !== null) {
-        candidate.classList.add('searcher_candidate')
-        console.log('adding class', candidate)
-        candidate.scrollIntoView({behavior: 'smooth', block: 'center'})
-    }
-    return anchors
+const validNode = (node) => {
+    // no scripts, styles, svg, or canvas
+    return node.tagName !== 'SCRIPT' && node.tagName !== 'STYLE' && node.tagName !== 'SVG' && node.tagName !== 'CANVAS'
 }
 
-function handleMessage(request, sender, sendResponse){
+const isNodeVisible = (node) => {
+    const style = window.getComputedStyle(node)
+    return style.display !== 'none' && style.visibility !== 'hidden'
+}
+
+const pushInOrder = (arr, item) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].index >= item.index) {
+            arr.splice(i, 0, item)
+            return
+        }
+    }
+    arr.push(item)
+
+}
+
+const find = (text) => {
+    const dfs = Array.from(document.children)
+    const candidates = []
+    while (dfs.length > 0) {
+        const node = dfs.pop();
+        if (validNode(node)) {
+            const fuzzyIndex = simpleSearch(node.textContent, text)
+            if (fuzzyIndex !== -1) {
+                pushInOrder(candidates, {node, index: fuzzyIndex})
+                dfs.push(...node.children)
+            }
+        }
+    }
+    console.log(candidates)
+    for (let i = 0; i < candidates.length; i++) {
+        if (isNodeVisible(candidates[i].node)) {
+            console.log('adding class', candidate)
+            candidate?.node.classList.remove('searcher_candidate')
+            candidate = candidates[i]
+            candidate.node.classList.add('searcher_candidate')
+            candidate.node.scrollIntoView({behavior: 'smooth', block: 'center'})
+            console.log('selected', candidates[i].node)
+            return
+        } else {
+            console.log('not visible', candidates[i].node)
+        }
+    }
+    console.log('no candidates')
+    candidate?.node.classList.remove('searcher_candidate')
+    candidate = null
+}
+
+function handleMessage(request, sender, sendResponse) {
     console.log("Message from the background script:");
     console.log(request);
-    if (request.action === 'filter') {
-        find(request.text)
+    if (request.action === 'filter' && request.text.length > 3) {
+        find(request.text.toLowerCase())
     } else if (request.action === 'goto') {
         if (candidate !== null) {
-            candidate.click()
+            candidate.node.click()
         }
     }
 
